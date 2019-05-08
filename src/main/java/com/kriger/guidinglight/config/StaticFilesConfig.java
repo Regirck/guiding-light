c12c321830b2cc957f79635d09311667fd84eab2
gaborKriger <regirck@gmail.com>
2019-05-08 14:54:42 +0200

    static file configure

---
+++ src/main/java/com/kriger/guidinglight/config/StaticFilesConfig.java
@@ -0,0 +1,19 @@
+package com.kriger.guidinglight.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class StaticFilesConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+}
