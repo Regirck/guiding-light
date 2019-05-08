5b143210255ea321ac9fc69f2d98e9c1ac2ab8ca
gaborKriger <regirck@gmail.com>
2019-05-08 09:14:29 +0200

    create repository

---
+++ src/main/java/com/kriger/guidinglight/GuidingLightApplication.java
@@ -0,0 +1,13 @@
+package com.kriger.guidinglight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidingLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuidingLightApplication.class, args);
+    }
+
+}
