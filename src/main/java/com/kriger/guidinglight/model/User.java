cc7c47c17ec808423b5a777d077788b0367853d8
gaborKriger <regirck@gmail.com>
2019-05-08 13:07:33 +0200

    create postgres database on the heroku and connect

--- src/main/java/com/kriger/guidinglight/model/User.java
+++ src/main/java/com/kriger/guidinglight/model/User.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
