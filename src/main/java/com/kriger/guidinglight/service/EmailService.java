fd0419d8fd5e5c9944257443cbc22f6b79d292c8
gaborKriger <regirck@gmail.com>
2019-05-20 17:16:24 +0200

    hotfix email /4

--- src/main/java/com/kriger/guidinglight/service/EmailService.java
+++ src/main/java/com/kriger/guidinglight/service/EmailService.java
@@ -12,35 +12,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-//    @Value("${spring.mail.username}")
-//    private String MESSAGE_FROM;
-//
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-//
-//    public void sendMessage(User user) {
-//        SimpleMailMessage message;
-//
-//        try {
-//            message = new SimpleMailMessage();
-//            message.setFrom(MESSAGE_FROM);
-//
-//            message.setTo(user.getEmail());
-//
-//            message.setSubject("Registration on the guiding light");
-//
-//            message.setText("Dear " + user.getEmail() + "! \n \n " +
-//                    "Thank you for the registration! \n \n " +
-//                    "Please activation your profile: \n " +
-//                    "\t https://guiding-light.herokuapp.com/activation/" + user.getActivation());
-//
-//            javaMailSender.send(message);
-//
-//        } catch (Exception e) {
-//            log.error("Error e-mail send: " + user.getEmail() + "  " + e);
-//        }
-//
-//
-//    }
+    @Value("${spring.mail.username}")
+    private String MESSAGE_FROM;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMessage(User user) {
+        SimpleMailMessage message;
+
+        try {
+            message = new SimpleMailMessage();
+            message.setFrom(MESSAGE_FROM);
+
+            message.setTo(user.getEmail());
+
+            message.setSubject("Registration on the guiding light");
+
+            message.setText("Dear " + user.getEmail() + "! \n \n " +
+                    "Thank you for the registration! \n \n " +
+                    "Please activation your profile: \n " +
+                    "\t https://guiding-light.herokuapp.com/activation/" + user.getActivation());
+
+            javaMailSender.send(message);
+
+        } catch (Exception e) {
+            log.error("Error e-mail send: " + user.getEmail() + "  " + e);
+        }
+
+
+    }
 
 }
