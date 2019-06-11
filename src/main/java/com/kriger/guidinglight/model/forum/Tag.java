d8cba00e6b9dd2dcf1a0134f8f21de79144cc711
Kriger Gábor <regirck@gmail.com>
2019-06-11 11:23:55 +0200

    forum model

---
+++ src/main/java/com/kriger/guidinglight/model/forum/Tag.java
@@ -0,0 +1,28 @@
+package com.kriger.guidinglight.model.forum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String tagName;
+
+    @ManyToOne
+    private Question question;
+
+
+}
