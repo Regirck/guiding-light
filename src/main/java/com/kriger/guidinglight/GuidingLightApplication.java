ad7da8bca74fbbc3c34c4ba310aa84656ef132de
Kriger Gábor <regirck@gmail.com>
2019-06-21 16:45:59 +0200

    deploy

--- src/main/java/com/kriger/guidinglight/GuidingLightApplication.java
+++ src/main/java/com/kriger/guidinglight/GuidingLightApplication.java
@@ -1,9 +1,7 @@
 package com.kriger.guidinglight;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GuidingLightApplication {
