39f7ea1c6ebae1b36de02ceb13d84a51220f309d
Kriger Gábor <regirck@gmail.com>
2019-08-09 12:36:03 +0200

    forum modal move question

--- src/main/resources/static/js/forum/forum.js
+++ src/main/resources/static/js/forum/forum.js
@@ -1,29 +1,3 @@
-function createModalWithUserPersonalData() {
-
-    let btn = document.querySelector("#modal-button");
-
-    btn.addEventListener("click", function () {
-        callUser();
-        openModal();
-    });
-
-    function callUser() {
-        let userId = document.querySelector("#user-id").innerHTML;
-        console.log(userId);
-        $.getJSON('/profile/get/' + userId, function(response) {
-            console.log(response);
-        });
-    }
-
-    function openModal() {
-        $("#user-modal").modal("show");
-    }
-
-}
-
-createModalWithUserPersonalData();
-
-
 function newQuestion() {
     location.href = "forum/new_question";
 }
--- src/main/resources/static/js/forum/question.js
+++ src/main/resources/static/js/forum/question.js
@@ -1,15 +1,34 @@
+function createModalWithUserPersonalData() {
+
+    let btn = document.querySelector("#modal-button");
+
+    btn.addEventListener("click", function () {
+        callUser();
+        openModal();
+    });
+
+    function callUser() {
+        let userId = document.querySelector("#user-id").innerHTML;
+        console.log(userId);
+        $.getJSON('/profile/get/' + userId, function(response) {
+            console.log(response);
+        });
+    }
+
+    function openModal() {
+        $("#user-modal").modal("show");
+    }
+}
+
+createModalWithUserPersonalData();
+
 function newAnswer() {
 
     let pathName = window.location.pathname;
 
-    console.log(pathName);
-
     let questionId = pathName.substr(16, pathName.length);
 
-    console.log(questionId);
-
     location.href = "/forum/question/new_answer/" + questionId;
-
 }
 
 
